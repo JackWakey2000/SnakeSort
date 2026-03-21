3fdf3b78aaf7b106265d69e29857114439dc7d7a
Jack Langdale <jackalangdale@gmail.com>
2026-03-21 18:20:18 +0000

    Started working on the main part of main.py and added os.path to required packages

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,5 +1,26 @@
 # Imports
 from os import *
+from os.path import *
 from termcolor import *
 
+# Welcome
 cprint("Welcome to SnakeSort!", attrs=["bold", "underline"])
+
+# Getting pathToSort
+pathToSort = input("What is the path of the directory you want to sort?")
+if pathToSort[-1] != "/":
+    pathToSort + "/"
+
+# List files/dirs in pathToSort
+pathArr = listdir(pathToSort)
+
+# Set arrays
+filesArr = []
+dirArr = []
+
+# Check if files are file/dir
+for i in pathArr:
+    if isfile(pathToSort + pathArr[i]):
+        filesArr.append(pathArr[i])
+    else:
+        dirArr.append(pathArr[i])
