d61b712ef241ff1e2887712e47f64e4f4c0db31c
Jack Langdale <jackalangdale@gmail.com>
2026-03-22 18:34:57 +0000

    🥳 1.0 RELEASE!

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 <div align="center">
 
-  <a href="">![version: unfinished](https://img.shields.io/badge/version-unfinished-red)</a>
+  <a href="">![version: unfinished](https://img.shields.io/badge/version-1.0-red)</a>
   <a href="">![python version: 3.14.0](https://img.shields.io/badge/python_version-3.14.0-%233776AB?logo=python&logoColor=%23FFFFFF
   )</a>
 
@@ -12,12 +12,17 @@
 ### Q. Why are you using os.system, which is deprecated?
 A. Mobile users do not have accses to subproccess.run()
 
+### Q. Help! Im getting a FileExistsError 
+A. Make sure your none of your folders are named "Directories"
 
 ## Required Python Packages
 - os (bundled with python)
 - os.path (bundled with python)
+- shutil (bundled with python)
 - termcolor
 
 
-
-SnakeSort is written and tested on Python 3.14. Feel free to test on other versions and do a PR to add here if they work!
+## Disclaimer
+- Snakesort may break symlinks.
+- Snakesort is tested on windows. Might have limited functionality on linux.
+- SnakeSort is written and tested on Python 3.14. Feel free to test on other versions and do a PR to add here if they work!
--- Scripts/main.py
+++ Scripts/main.py
@@ -2,8 +2,9 @@
 from os import *
 from os.path import *
 from termcolor import *
+from shutil import *
 
-# set "clear" var
+# Set "clear" var
 if name == "nt":
     clear = "cls"
 else:
@@ -13,27 +14,89 @@ else:
 system(clear)
 cprint("Welcome to SnakeSort!", attrs=["bold", "underline"])
 
-# Getting pathToSort
+# Getting pathToSort & Confirming
 pathToSort = input("What is the path of the directory you want to sort? ")
 if pathToSort[-1] != "/" and pathToSort[-1] != "\\":
     pathToSort += "/"
+    print(pathToSort)
+if input("Are you sure? (Y/n) ") == "n":
+    raise SystemExit
 
 # List files/dirs in pathToSort
 pathArr = listdir(pathToSort)
 
-# Set arrays
-filesArr = []
-dirArr = []
-
-# Check if files are file/dir
-for i in range(len(pathArr)):
-    if isfile(pathToSort + pathArr[i]):
-        filesArr.append(pathArr[i])
-    else:
-        dirArr.append(pathArr[i])
-
-# Print the two arrs
+# Sorting the files
 system(clear)
-print(f"Directory sorted: {pathToSort}\n")
-print(f"Files: {filesArr}\n")
-print(f"Dirs: {dirArr}\n")
+print("Sorting...")
+for i in range(len(pathArr)):
+    checkNow = pathArr[i]
+    if isdir(pathToSort + checkNow):
+        try:
+            mkdir(f"{pathToSort}/Directories")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Directories/{checkNow}")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Directories/{checkNow}")
+    elif checkNow.lower().endswith(".txt"):
+        try:
+            mkdir(f"{pathToSort}/Plain Text")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Plain Text")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Plain Text")
+    elif checkNow.lower().endswith((".png", ".jpg", ".jpeg", ".svg", ".webp", ".gif")):
+        try:
+            mkdir(f"{pathToSort}/Images")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Images")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Images")
+    elif checkNow.lower().endswith((".mp4", ".mov", ".mkv", ".avi", ".webm")):
+        try:
+            mkdir(f"{pathToSort}/Videos")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Videos")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Videos")
+    elif checkNow.lower().endswith((".mp3", ".wav", ".ogg", ".aac", ".flac")):
+        try:
+            mkdir(f"{pathToSort}/Audio")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Audio")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Audio")
+    elif checkNow.lower().endswith((".py", ".js", ".java", ".cs", ".cpp", ".json", ".ts")):
+        try:
+            mkdir(f"{pathToSort}/Scripts")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Scripts")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Scripts")
+    elif checkNow.lower().endswith(".lnk"):
+        try:
+            mkdir(f"{pathToSort}/Links")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Links")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Links")
+    elif checkNow.lower().endswith(".exe"):
+        try:
+            mkdir(f"{pathToSort}/Executables")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Executables")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Executables")
+    elif checkNow.lower().endswith(".bin"):
+        try:
+            mkdir(f"{pathToSort}/Binaries")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Binaries")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Binaries")
+    elif checkNow.lower().endswith(".dll"):
+        try:
+            mkdir(f"{pathToSort}/DLLs")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/DLLs")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/DLLs")
+    else:
+        try:
+            mkdir(f"{pathToSort}/Other (Unknown extention)")
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Other (Unknown extention)")
+        except FileExistsError:
+            move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Other (Unknown extention)")
+
+# Final print
+system(clear)
+print(f"Directory sorted: {pathToSort}")
