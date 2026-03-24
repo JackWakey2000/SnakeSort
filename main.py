d40ef562d76137a641ee193379ff5630066c7885
Jack Langdale <jackalangdale@gmail.com>
2026-03-24 19:05:21 +0000

    Commented main.py more

--- main.py
+++ main.py
@@ -24,71 +24,93 @@ if input("Are you sure? (Y/n) ") == "n":
 # List files/dirs in pathToSort
 pathArr = listdir(pathToSort)
 
-# Sorting the files
-system(clear)
-print("Sorting...")
+# Iterate through each file
 for i in range(len(pathArr)):
+
+    # Get the current element
     checkNow = pathArr[i]
+
+    # Check if directory
     if isdir(pathToSort + checkNow):
         try:
             mkdir(f"{pathToSort}/Directories")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Directories/{checkNow}")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Directories/{checkNow}")
+
+    # Check if plain text
     elif checkNow.lower().endswith(".txt"):
         try:
             mkdir(f"{pathToSort}/Plain Text")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Plain Text")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Plain Text")
+
+    # Check if image
     elif checkNow.lower().endswith((".png", ".jpg", ".jpeg", ".svg", ".webp", ".gif")):
         try:
             mkdir(f"{pathToSort}/Images")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Images")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Images")
+    
+    # Check if video
     elif checkNow.lower().endswith((".mp4", ".mov", ".mkv", ".avi", ".webm")):
         try:
             mkdir(f"{pathToSort}/Videos")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Videos")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Videos")
+
+    # Check if audio
     elif checkNow.lower().endswith((".mp3", ".wav", ".ogg", ".aac", ".flac")):
         try:
             mkdir(f"{pathToSort}/Audio")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Audio")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Audio")
+
+    # Check if script
     elif checkNow.lower().endswith((".py", ".js", ".java", ".cs", ".cpp", ".json", ".ts")):
         try:
             mkdir(f"{pathToSort}/Scripts")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Scripts")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Scripts")
+
+    # Check if link
     elif checkNow.lower().endswith(".lnk"):
         try:
             mkdir(f"{pathToSort}/Links")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Links")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Links")
+    
+    # Check if executable
     elif checkNow.lower().endswith(".exe"):
         try:
             mkdir(f"{pathToSort}/Executables")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Executables")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Executables")
+
+    # Check if binary
     elif checkNow.lower().endswith(".bin"):
         try:
             mkdir(f"{pathToSort}/Binaries")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Binaries")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/Binaries")
+
+    # Check if DLL
     elif checkNow.lower().endswith(".dll"):
         try:
             mkdir(f"{pathToSort}/DLLs")
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/DLLs")
         except FileExistsError:
             move(f"{pathToSort}/{checkNow}", f"{pathToSort}/DLLs")
+
+    # Other
     else:
         try:
             mkdir(f"{pathToSort}/Other (Unknown extention)")
